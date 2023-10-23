1467d58b4c3295f7d8f39e24bbe30a4bd8d7d2e5
diffficult <spaulo@gmail.com>
2023-10-23 18:58:19 -0300

    🧱 tweaked icons and script

--- i3/.config/i3/myblocks/iconweather.sh
+++ i3/.config/i3/myblocks/iconweather.sh
@@ -14,6 +14,11 @@ fi
 #  3) set locCode="SAM|AR|AR007|BUENOS AIRES" ;; # right click Buenos Aires, Argentina
 #esac
 # $sun = "";
+# $cloud = "";
+# $rain = "";
+# $wind = "";
+# $snow = "";
+# $night = "";
 
 curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'use utf8; if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; my @values=split(":",$1); if( $values[0] eq "Sunny" || $values[0] eq "Mostly Sunny" || $values[0] eq "Partly Sunny" || $values[0] eq "Intermittent Clouds" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hazy Sunshine" || $values[0] eq "Hot")
 {
@@ -23,31 +28,31 @@ print "$sun";
 }
 if( $values[0] eq "Mostly Cloudy" || $values[0] eq "Cloudy" || $values[0] eq "Dreary (Overcast)" || $values[0] eq "Fog")
 {
-my $cloud = "";
+my $cloud = "☁️";
 binmode(STDOUT, ":utf8");
 print "$cloud";
 }
 if( $values[0] eq "Showers" || $values[0] eq "Mostly Cloudy w/ Showers" || $values[0] eq "Partly Sunny w/ Showers" || $values[0] eq "T-Storms"|| $values[0] eq "Mostly Cloudy w/ T-Storms"|| $values[0] eq "Partly Sunny w/ T-Storms"|| $values[0] eq "Rain")
 {
-my $rain = "";
+my $rain = "🌧️";
 binmode(STDOUT, ":utf8");
 print "$rain";
 }
 if( $values[0] eq "Windy")
 {
-my $wind = "";
+my $wind = "🍃";
 binmode(STDOUT, ":utf8");
 print "$wind";
 }
 if($values[0] eq "Flurries" || $values[0] eq "Mostly Cloudy w/ Flurries" || $values[0] eq "Partly Sunny w/ Flurries"|| $values[0] eq "Snow"|| $values[0] eq "Mostly Cloudy w/ Snow"|| $values[0] eq "Ice"|| $values[0] eq "Sleet"|| $values[0] eq "Freezing Rain"|| $values[0] eq "Rain and Snow"|| $values[0] eq "Cold")
 {
-my $snow = "";
+my $snow = "❄️";
 binmode(STDOUT, ":utf8");
 print "$rain";
 }
 if($values[0] eq "Clear" || $values[0] eq "Mostly Clear" || $values[0] eq "Partly Cloudy"|| $values[0] eq "Intermittent Clouds"|| $values[0] eq "Hazy Moonlight"|| $values[0] eq "Mostly Cloudy"|| $values[0] eq "Partly Cloudy w/ Showers"|| $values[0] eq "Mostly Cloudy w/ Showers"|| $values[0] eq "Partly Cloudy w/ T-Storms"|| $values[0] eq "Mostly Cloudy w/ Flurries" || $values[0] eq "Mostly Cloudy w/ Snow")
 {
-my $night = "";
+my $night = "🌜";
 binmode(STDOUT, ":utf8");
 print "$night";
 }
